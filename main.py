d828e315f8eee4b1df145d686129aff7d3fde22f
Quotermain <>
2021-03-10 17:43:45 +0500

    Writes prices to file

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from utils.get_candles import get_candles
 from utils.load_pickle_object import load_pickle_object
-from utils.populate_prices_table import populate_prices_table
-
-from time import sleep
+from multiprocessing import Pool
+import pickle
 
 ALL_TICKERS = load_pickle_object('data/tickers/all_tickers.pickle')
 
 def run(ticker):
     print(ticker)
-    prices = get_candles(ticker, 'mt5.TIMEFRAME_M1', 500).tolist()
-    print(prices)
-    populate_prices_table(ticker, prices)
+    prices = get_candles(ticker, 'mt5.TIMEFRAME_M1', 100000)
+    with open(f'data/prices/{ticker}.pickle', 'wb') as file:
+        pickle.dump(prices, file)
 
 if __name__ == '__main__':
     while True:
-        for ticker in ALL_TICKERS:
-            run(ticker)
+        try:
+            with Pool(4) as p:
+                p.map(run, ALL_TICKERS)
+        except KeyboardInterrupt:
+            print('Aborting')
+        except Exception:
+            continue
