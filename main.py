d5c54be65ad4d97d8edd32550eb36274a6a8b4e4
Quotermain <>
2021-04-19 18:52:01 +0500

    Gets prices for ALL tqbr tickers

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from utils.get_candles import get_candles
-from utils.load_pickle_object import load_pickle_object
+from utils.get_tqbr_tickers import get_tqbr_tickers
 from multiprocessing import Pool
 import pickle
 
-ALL_TICKERS = load_pickle_object('data/tickers/all_tickers.pickle')
+ALL_TICKERS = get_tqbr_tickers()
 
 def run(ticker):
-    print(ticker)
+    #print(ticker)
     prices = get_candles(ticker, 'mt5.TIMEFRAME_M1', 100000)
     with open(f'data/prices/{ticker}.pickle', 'wb') as file:
         pickle.dump(prices, file)
 
 if __name__ == '__main__':
-    while True:
-        try:
-            with Pool(4) as p:
-                p.map(run, ALL_TICKERS)
-        except KeyboardInterrupt:
-            print('Aborting')
-        except Exception:
-            continue
+    #while True:
+    try:
+        with Pool(4) as p:
+            p.map(run, ALL_TICKERS)
+    except KeyboardInterrupt:
+        print('Aborting')
+    except Exception as e:
+        print(e)
+        #continue
